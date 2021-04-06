352fb0ffbe7ccfea4dd2a13d3335eed2471fbbc6
Andrej1198 <andrej.glavic.can@gmail.com>
2021-04-06 11:38:32 -0400

    Added code to stroop test to save data as csv, also added file called pandas_read_csv which merges all the csv data into one big pandas dataframe

---
+++ pandas_read_csv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+# Get all the files in directory
+files = os.listdir()
+
+# Identify all csv files in the directory
+csv_files = []
+for file in files:
+    if file[-4:] == ".csv":
+        csv_files.append(file)
+data = pd.DataFrame()
+for file in csv_files:
+    data = data.append(pd.read_csv(file))
+
+print(data)
+# Data is a pandas dataframe that has all the data from the csv files merged into 1 big data frame
